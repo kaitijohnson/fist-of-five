156067f53535271fd23d4124d0ca9b3a0c289433
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-04-08 15:27:55 -0600

    "have working average"

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -9,6 +9,23 @@ $(() => {
   })
 
   socket.on('session object', data => {
-    ul.append(`<li>${data.happy.length} are happy, ${data.ya.length} are ya,${data.meh.length} are meh,${data.confused.length} are confused,${data.angry.length} are angry, </li>`)
+    //     ul.append(`<li>${data.happy.students.length} are happy, ${data.ya.students.length} are ya,${data.meh.students.length} are meh,${data.confused.students.length} are confused,${data.angry.students.length} are angry,
+    // ${averageRating(data)}
+    //        </li>`)
+
+    ul.prepend(`<li>This is the class average: ${averageRating(data)}</li>`)
   })
 })
+
+function averageRating(data) {
+  // console.log(data);
+  let sum = 0
+  let studentSum = 0
+  for (var mood in data) {
+    let currentMood = data[mood]
+    console.log(currentMood.value, '-----', currentMood.students.length);
+    sum += currentMood.value * currentMood.students.length
+    studentSum += currentMood.students.length
+  }
+  return (sum / studentSum).toFixed(2)
+}
