d7570492f2b4cc546498ebdd44b8c6ce33fb1d92
robotsintheattic <katherine.r.johnson@gmail.com>
2017-04-08 14:13:54 -0600

    set boilerplate routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Routes Boilerplate. */
 router.get('/', function(req, res, next) {
   res.render('sockettest', {
     title: 'Express'
   });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('index', { title: 'The Signup Page' });
+});
+
+router.get('/dashboard/:id', function(req, res, next) {
+  res.render('index', { title: 'This is an individual user\'s dashboard' });
+});
+
+/* session route will be re-written after socket is configured*/
+router.get('/session/:id', function(req, res, next) {
+  res.render('index', { title: 'This is a class session' });
 });
 
 module.exports = router;
