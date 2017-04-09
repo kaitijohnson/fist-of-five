f57e79c9cb9c79d05247aa7e21cd5a6435fef3f9
spkelly <spkelly18@gmail.com>
2017-04-09 17:59:42 -0600

    added button to instructor dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,6 +18,7 @@ router.get('/:id', verifyToken, getClasses, function(req, res, next) {
     })
 });
 
+<<<<<<< HEAD
 router.post('/:id', verifyClassName, function(req, res, next) {
   let userId = req.params.id;
   console.log("ClassName", req.body.className);
@@ -31,6 +32,15 @@ router.post('/:id', verifyClassName, function(req, res, next) {
       });
     })
 
+=======
+router.post('/:id', verifyClassName, function(req,res,next){
+  console.log("ClassName",req.body.className);
+  insertClass(req.body.className)
+    .then((data) =>{
+      console.log(data);
+      res.render('dashboard');
+    })
+>>>>>>> added button to instructor dashboard
 })
 
 function getClasses(req, res, next) {
@@ -54,6 +64,7 @@ function verifyToken(req, res, next) {
   });
 }
 
+<<<<<<< HEAD
 function verifyClassName(req, res, next) {
   if (!req.body.className || !req.body.className.trim()) {
     next(boom.create(400, "no class name provided"));
@@ -73,6 +84,18 @@ const addtoUsersClasses = (userId, classID) => {
 const insertClass = (className) => knex('classes').returning('*').insert({
   'name': className
 });
+=======
+function verifyClassName(req,res,next){
+  if(!req.body.className|| !req.body.className.trim()){
+    next(boom.create(400, "no class name provided"));
+  }
+  else{
+    next();
+  }
+}
+const addtoUsersClasses = (instructorID,classID)
+const insertClass = (className) => knex('classes').returning('*').insert( { 'name': className } );
+>>>>>>> added button to instructor dashboard
 const getUserClasses = (id) => knex('users').where('id', id)
 const getAllClasses = () => knex('classes')
 
