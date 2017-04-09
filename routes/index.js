609fd5b18b73be253693ca842a8d74b1445a93da
robotsintheattic <katherine.r.johnson@gmail.com>
2017-04-08 21:49:33 -0600

    added template to splash and lofin pages

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* Routes Boilerplate. */
 router.get('/', function(req, res, next) {
-  res.render('sockettest', {
-    title: 'Express'
-  });
+  res.render('splash', { title: 'The Splash Page' });
 });
 
 router.get('/signup', function(req, res, next) {
@@ -17,10 +15,16 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/dashboard/:id', function(req, res, next) {
-  res.render('index', { title: 'The individual user\'s dashboard' });
+  res.render('index', { title: 'The Individual User\'s Dashboard' });
 });
 
 /* session route will be re-written after socket is configured*/
+router.get('/classroom', function(req, res, next) {
+  res.render('sockettest', {
+    title: 'Express'
+  });
+});
+
 router.get('/session/:id', function(req, res, next) {
   res.render('index', { title: 'A class session' });
 });
