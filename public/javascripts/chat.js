1717ca90cae36fd23270c93a578284d9874a1557
spkelly <spkelly18@gmail.com>
2017-04-10 13:12:30 -0600

    seperate classrooms now connect to seperate socket rooms

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,19 +1,39 @@
 $(() => {
+  console.log();
+  let = urlArray = $(location).attr('href').split('/');
+  let classId = urlArray[urlArray.length -1];
+  let currentRoom = `${classId}_classroom`
+
+
+  while(isNaN(parseInt(classId))){
+    urlArray.pop()
+    console.log(urlArray);
+    classId = urlArray[urlArray.length -1];
+    console.log(classId);
+  }
+
+  console.log(classId);
   let socket = io()
   let ul = $('#messages')
 
   $('.rating').click((e) => {
     e.preventDefault()
     let input = $(e.target)
-    socket.emit('mood', input.attr('data-value'))
+    socket.emit('mood', {mood:input.attr('data-value'),room:currentRoom})
   })
-
   socket.on('session object', data => {
     //     ul.append(`<li>${data.happy.students.length} are happy, ${data.ya.students.length} are ya,${data.meh.students.length} are meh,${data.confused.students.length} are confused,${data.angry.students.length} are angry,
     // ${averageRating(data)}
     //        </li>`)
 
     ul.prepend(`<li>This is the class average: ${averageRating(data)}</li>`)
+  })
+  socket.on('findRoom', data =>{
+    socket.emit('joinRoom', currentRoom);
+  })
+  socket.on('message', data =>{
+    console.log("got something");
+    ul.prepend(`<li>${data}</li>`)
   })
 })
 
