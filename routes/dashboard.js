414048d70709940a479d3f13f49d7d5d3b3b468a
spkelly <spkelly18@gmail.com>
2017-04-09 19:03:37 -0600

    created classes are now added to users_classes join table

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,7 +18,6 @@ router.get('/:id', verifyToken, getClasses, function(req, res, next) {
     })
 });
 
-<<<<<<< HEAD
 router.post('/:id', verifyClassName, function(req, res, next) {
   let userId = req.params.id;
   console.log("ClassName", req.body.className);
@@ -31,16 +30,6 @@ router.post('/:id', verifyClassName, function(req, res, next) {
         classes
       });
     })
-
-=======
-router.post('/:id', verifyClassName, function(req,res,next){
-  console.log("ClassName",req.body.className);
-  insertClass(req.body.className)
-    .then((data) =>{
-      console.log(data);
-      res.render('dashboard');
-    })
->>>>>>> added button to instructor dashboard
 })
 
 function getClasses(req, res, next) {
@@ -64,7 +53,6 @@ function verifyToken(req, res, next) {
   });
 }
 
-<<<<<<< HEAD
 function verifyClassName(req, res, next) {
   if (!req.body.className || !req.body.className.trim()) {
     next(boom.create(400, "no class name provided"));
@@ -84,18 +72,6 @@ const addtoUsersClasses = (userId, classID) => {
 const insertClass = (className) => knex('classes').returning('*').insert({
   'name': className
 });
-=======
-function verifyClassName(req,res,next){
-  if(!req.body.className|| !req.body.className.trim()){
-    next(boom.create(400, "no class name provided"));
-  }
-  else{
-    next();
-  }
-}
-const addtoUsersClasses = (instructorID,classID)
-const insertClass = (className) => knex('classes').returning('*').insert( { 'name': className } );
->>>>>>> added button to instructor dashboard
 const getUserClasses = (id) => knex('users').where('id', id)
 const getAllClasses = () => knex('classes')
 
