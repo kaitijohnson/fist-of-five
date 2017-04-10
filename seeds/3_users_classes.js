dadcd7d116ee7ef6bdc7508bfb8e16155b7e4426
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-04-09 19:48:26 -0600

    "fixed seeds"

--- seeds/3_users_classes.js
+++ seeds/3_users_classes.js
@@ -6,19 +6,24 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('users_classes').insert([{
           id: 1,
-          user_id: '2'
+          user_id: 2,
+          class_id: 1
         }, {
           id: 2,
-          user_id: '1'
+          user_id: 1,
+          class_id: 1
         }, {
           id: 3,
-          user_id: '3'
+          user_id: 3,
+          class_id: 1
         }, {
           id: 4,
-          user_id: '4'
+          user_id: 4,
+          class_id: 1
         }, {
           id: 5,
-          user_id: '5'
+          user_id: 5,
+          class_id: 1
         }])
 
       ])
