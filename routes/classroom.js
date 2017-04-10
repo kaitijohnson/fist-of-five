368290a7e7f073641c88e2c25a4bb99268c6d2e2
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-04-09 20:15:34 -0600

    "boilerplate classroom file"

--- routes/classroom.js
+++ routes/classroom.js
@@ -15,42 +15,5 @@ router.get('/:id', function(req, res, next) {
   })
 });
 
-function ioFunction() {
-  let sessionObject = {
-    happy: {
-      value: 5,
-      students: []
-    },
-    ya: {
-      value: 4,
-      students: []
-    },
-    meh: {
-      value: 3,
-      students: []
-    },
-    confused: {
-      value: 2,
-      students: []
-    },
-    angry: {
-      value: 1,
-      students: []
-    }
-  }
-
-  app.io.on('connection', function(socket) {
-    console.log("someone entered");
-
-    socket.on('disconnect', () => {
-      console.log("user disconnected");
-    })
-
-    socket.on('mood', data => {
-      sessionObject[data].students.push('student')
-      app.io.emit('session object', sessionObject)
-    })
-  });
-}
 
 module.exports = router;
