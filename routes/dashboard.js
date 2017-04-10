6675a0a690bf900ecd8eb8eab0723dae9f374e43
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-04-09 19:46:32 -0600

    "refactored test, added dashboard knex functinoality"

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,17 +8,21 @@ const jwt = require('jsonwebtoken');
 
 router.get('/:id', verifyToken, getClasses, function(req, res, next) {
   console.log(req.params.id);
-  res.render('index', {
-    title: `The individual user\'s dashboard ${req.params.id}`
-  });
+  getAllClasses()
+    .then(classes => {
+      res.render(`/dashboard/${req.params.id}`, {
+        title: `The individual user\'s dashboard ${req.params.id}`,
+        classes
+      });
+    })
 });
 
-router.post('/:id', verifyClassName, function( req, res, next){
+router.post('/:id', verifyClassName, function(req, res, next) {
   let userId = req.params.id;
-  console.log("ClassName",req.body.className);
+  console.log("ClassName", req.body.className);
   insertClass(req.body.className)
     .then((data) => addtoUsersClasses(userId, data[0].id))
-    .then((userClassRow) =>{
+    .then((userClassRow) => {
       console.log(userClassRow);
       res.render('dashboard');
     })
@@ -35,30 +39,35 @@ function verifyToken(req, res, next) {
   jwt.verify(req.cookies.token, 'shhh', function(err, decoded) {
     console.log(Object.keys(decoded));
     if (decoded.id == req.params.id) {
-      res.render('dashboard', {
-        title: decoded.id
-      })
+      next()
     } else {
       res.clearCookie('token')
-      res.redirect(`/dashboard/${decoded.id}`)
+      res.redirect(`/login`)
     }
 
   });
 }
 
-function verifyClassName(req,res,next){
-  if(!req.body.className|| !req.body.className.trim()){
+function verifyClassName(req, res, next) {
+  if (!req.body.className || !req.body.className.trim()) {
     next(boom.create(400, "no class name provided"));
-  }
-  else{
+  } else {
     next();
   }
 }
-const addtoUsersClasses = (userId,classID) => {
+
+const addtoUsersClasses = (userId, classID) => {
   return knex('users_classes')
-  .returning('*')
-  .insert({ 'class_id' : classID, 'user_id': userId });
+    .returning('*')
+    .insert({
+      'class_id': classID,
+      'user_id': userId
+    });
 }
-const insertClass = (className) => knex('classes').returning('*').insert( { 'name': className } );
+const insertClass = (className) => knex('classes').returning('*').insert({
+  'name': className
+});
 const getUserClasses = (id) => knex('users').where('id', id)
+const getAllClasses = () => knex('users')
+
 module.exports = router;
