f678077456e2d4ad32b396a18f5c84dece6f1a42
matthewalbrecht <MatthewWilliamAlbrecht@gmail.com>
2017-04-09 20:39:38 -0600

    "trying to get io into routes"

--- routes/classroom.js
+++ routes/classroom.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 const boom = require('boom');
 const knex = require('../knex');
 const bcrypt = require('bcrypt');
@@ -7,10 +8,49 @@ const humps = require('humps');
 const jwt = require('jsonwebtoken');
 
 router.get('/:id', function(req, res, next) {
-  res.render('classroom', {
+  console.log(router);
+  ioFunction()
+  res.render('sockettest', {
     title: req.params.id
   })
 });
 
+function ioFunction() {
+  let sessionObject = {
+    happy: {
+      value: 5,
+      students: []
+    },
+    ya: {
+      value: 4,
+      students: []
+    },
+    meh: {
+      value: 3,
+      students: []
+    },
+    confused: {
+      value: 2,
+      students: []
+    },
+    angry: {
+      value: 1,
+      students: []
+    }
+  }
+
+  app.io.on('connection', function(socket) {
+    console.log("someone entered");
+
+    socket.on('disconnect', () => {
+      console.log("user disconnected");
+    })
+
+    socket.on('mood', data => {
+      sessionObject[data].students.push('student')
+      app.io.emit('session object', sessionObject)
+    })
+  });
+}
 
 module.exports = router;
